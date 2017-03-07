d16c17bb596c63b89abce71a7aff5c9c42d6531b
asainnp <asainnp@gmail.com>
2017-03-07 10:09:41 +0000

    .. in ..common.php

--- webcontrol/tvcmd.php
+++ webcontrol/tvcmd.php
@@ -1,5 +1,5 @@
 <?php 
-   require_once('../common.php');
+   require_once('common.php');
    
    $cmd  =getrq('cmd');  if ($cmd  === false) exit();
    $par1 =getrq('par1'); if ($par1 !== false) $cmd = "$cmd $par1";
@@ -10,4 +10,6 @@
    $_SESSION['omxtvlog'][] ="$cmd";
 
    header('Location: ' .$_SERVER['HTTP_REFERER']);
+
+   //header('refresh:1; url=' .$_SERVER['HTTP_REFERER']);
 ?>
