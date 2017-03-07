c3e30b66135997fd30cca896a2c1837ca58bd678
Asain Kujovic <asain.kujovic@bstelecom.ba>
2017-03-07 06:50:59 +0100

    tvcmd return to refferer

--- webcontrol/tvcmd.php
+++ webcontrol/tvcmd.php
@@ -1,9 +1,3 @@
-<head>
-<style>
-   body { font-size: 2em; }
-</style>
-</head>
-<body>
 <?php
    function getrq($key, $def=false) { return array_key_exists($key, $_REQUEST) ?$_REQUEST[$key] :$def; }
    
@@ -11,6 +5,6 @@
    $par1 =getrq('par1'); if ($par1 !== false) $cmd = "$cmd $par1";
 
    exec("echo $cmd > /tmp/omxtvstatuslog/maincmdfifo");
-   echo "sent command $cmd to " .file_get_contents("/etc/hostname");
+
+   header('Location: ' .$_SERVER['HTTP_REFERER']);
 ?>
-</body>
